0fde180b987f5b6391dfdb589384483829008fa0
Keu-ni <skcor8909@gmail.com>
2020-10-18 20:04:34 +0900

    create modules

---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// Let's create an instance
+let myCustomer = new Customer("Keu-ni", "Ahn");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
