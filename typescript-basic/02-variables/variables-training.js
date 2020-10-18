f09bf5338cb6c72584e485b9fe6edd5ac0d0af71
Keu-ni <skcor8909@gmail.com>
2020-10-18 15:28:21 +0900

    add file

---
+++ typescript-basic/02-variables/variables-training.js
@@ -0,0 +1,11 @@
+// let <variableName>: <type> = <initial value>;
+var found = true;
+var grade = 177.7;
+var firstName = "Keuni";
+var lastName = "Ahn";
+// "found" is not another type absolutely!
+// found = 0; <-- error 
+//Type: any - "any type can assign different value of other type."
+var myDate = 26.8;
+myDate = true;
+myDate = 15.6;
